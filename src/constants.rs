987827fcdf6b569983d4741d78d1153e18110ef2
Gabriel Hideki Izuka Moraes <gabriel.hideki@hotmail.com>
2026-02-16 00:35:09 -0300

    refactor: specify output row limit size for larger outputs

--- src/constants.rs
+++ src/constants.rs
@@ -3,3 +3,5 @@ pub const GAP_PENALTY: i32 = 2;
 pub const GAP_EXTENSION_PENALTY: i32 = 1;
 pub const MATCH_SCORE: i32 = 1;
 pub const MISMATCH_PENALTY: i32 = 3;
+
+pub const OUTPUT_ROW_SIZE: usize = 80;
